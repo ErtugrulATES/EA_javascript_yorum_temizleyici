c697dcbc7560fc3d13eb86096b34275f01241b49
ErtugrulATES <ertugrul.ates@outlook.com>
2017-01-02 12:43:58 +0300

    visual studio 2015 e yükseltildi

--- EA_javascript_yorum_temizleyici/Form1.Designer.vb
+++ EA_javascript_yorum_temizleyici/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,11 +20,101 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.girdi = New System.Windows.Forms.TextBox()
+        Me.cikti = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.DonusturToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CiktiyiKopyalaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ÇıktıyıYazdırToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'girdi
+        '
+        Me.girdi.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.girdi.Location = New System.Drawing.Point(12, 27)
+        Me.girdi.Multiline = True
+        Me.girdi.Name = "girdi"
+        Me.girdi.Size = New System.Drawing.Size(519, 75)
+        Me.girdi.TabIndex = 0
+        '
+        'cikti
+        '
+        Me.cikti.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cikti.Location = New System.Drawing.Point(12, 110)
+        Me.cikti.Multiline = True
+        Me.cikti.Name = "cikti"
+        Me.cikti.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.cikti.Size = New System.Drawing.Size(614, 566)
+        Me.cikti.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DonusturToolStripMenuItem1, Me.CiktiyiKopyalaToolStripMenuItem, Me.ÇıktıyıYazdırToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(638, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'DonusturToolStripMenuItem1
+        '
+        Me.DonusturToolStripMenuItem1.Name = "DonusturToolStripMenuItem1"
+        Me.DonusturToolStripMenuItem1.Size = New System.Drawing.Size(68, 20)
+        Me.DonusturToolStripMenuItem1.Text = "Dönüştür"
+        '
+        'CiktiyiKopyalaToolStripMenuItem
+        '
+        Me.CiktiyiKopyalaToolStripMenuItem.Name = "CiktiyiKopyalaToolStripMenuItem"
+        Me.CiktiyiKopyalaToolStripMenuItem.Size = New System.Drawing.Size(97, 20)
+        Me.CiktiyiKopyalaToolStripMenuItem.Text = "Çıktıyı Kopyala"
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(537, 27)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(89, 74)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Dosya Aç"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ÇıktıyıYazdırToolStripMenuItem
+        '
+        Me.ÇıktıyıYazdırToolStripMenuItem.Name = "ÇıktıyıYazdırToolStripMenuItem"
+        Me.ÇıktıyıYazdırToolStripMenuItem.Size = New System.Drawing.Size(86, 20)
+        Me.ÇıktıyıYazdırToolStripMenuItem.Text = "Çıktıyı Yazdır"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(638, 688)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.cikti)
+        Me.Controls.Add(Me.girdi)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Ertuğrul ATEŞ - Javascript Yorum Temizleyici"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents girdi As TextBox
+    Friend WithEvents cikti As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents DonusturToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CiktiyiKopyalaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ÇıktıyıYazdırToolStripMenuItem As ToolStripMenuItem
 End Class
--- EA_javascript_yorum_temizleyici/Form1.vb
+++ EA_javascript_yorum_temizleyici/Form1.vb
@@ -1,3 +1,120 @@
-﻿Public Class Form1
+﻿Imports System.Collections.ObjectModel
+Imports Microsoft.VisualBasic.FileIO
+Imports System
+Imports System.IO
+Imports System.Text
+Imports System.Windows.Forms
 
+Public Class Form1
+
+    Private Sub Donustur()
+        Dim path As String = girdi.Text
+        If (path = "" Or path = String.Empty) Then
+            Return
+        End If
+        cikti.Clear()
+
+        Try
+            Dim sr As StreamReader = New StreamReader(path)
+            Dim satir As String
+            Dim yardimciSatir As String = ""
+            Dim blokYorumu As Boolean = False
+            Dim boslukKontrol As String
+
+            Do While sr.Peek() >= 0
+                boslukKontrol = yardimciSatir.Trim
+                If boslukKontrol.Length = 0 Then
+
+                Else
+                    cikti.AppendText(yardimciSatir + vbNewLine)
+                End If
+
+                yardimciSatir = ""
+                satir = sr.ReadLine()
+
+
+                If (satir.Contains("//") Or satir.Contains("/*") Or satir.Contains("*/")) Then
+                    While (satir.Contains("//") Or satir.Contains("/*") Or satir.Contains("*/"))
+                        If blokYorumu Then
+                            If satir.Contains("*/") Then
+                                satir = satir.Substring(satir.IndexOf("*/") + 2, satir.Length - (satir.IndexOf("*/") + 2))
+                                blokYorumu = False
+                            Else
+                                Continue Do
+                            End If
+                        Else
+                            If satir.Contains("//") And satir.Contains("/*") Then
+                                If satir.IndexOf("//") < satir.IndexOf("/*") Then
+                                    yardimciSatir += satir.Substring(0, satir.IndexOf("//"))
+
+                                    blokYorumu = False
+                                    Continue Do
+                                Else
+                                    yardimciSatir += satir.Substring(0, satir.IndexOf("/*"))
+                                    satir = satir.Substring(satir.IndexOf("/*") + 2, satir.Length - (satir.IndexOf("/*") + 2))
+                                    blokYorumu = True
+                                End If
+                            ElseIf satir.Contains("//") Then
+                                yardimciSatir += satir.Substring(0, satir.IndexOf("//"))
+                                Continue Do
+                            ElseIf satir.Contains("/*") Then
+                                yardimciSatir += satir.Substring(0, satir.IndexOf("/*"))
+                                satir = satir.Substring(satir.IndexOf("/*") + 2, satir.Length - (satir.IndexOf("/*") + 2))
+                                blokYorumu = True
+                            Else
+
+
+                            End If
+                        End If
+                    End While
+                    If blokYorumu = False Then
+                        yardimciSatir += satir
+                    End If
+                Else
+                    If blokYorumu Then
+                        Continue Do
+                    Else
+                        yardimciSatir += satir
+                    End If
+                End If
+            Loop
+            cikti.AppendText(yardimciSatir + vbNewLine)
+            sr.Close()
+        Catch er As Exception
+            Console.WriteLine("The process failed: {0}", er.ToString())
+        End Try
+    End Sub
+    Private Sub DonusturToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles DonusturToolStripMenuItem1.Click
+        Donustur()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object,
+   ByVal e As System.EventArgs) Handles Button1.Click
+        ' dosya açma diyalog penceresini açar.
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.Filter = "Javascript Dosyaları|*.js"
+        openFileDialog1.Title = "Bir Javascript Dosyası Seçin"
+
+
+        If openFileDialog1.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            girdi.Text = openFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub CiktiyiKopyalaToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles CiktiyiKopyalaToolStripMenuItem.Click
+        If (cikti.Text = "" Or cikti.Text = String.Empty) Then
+            Return
+        End If
+        My.Computer.Clipboard.SetText(cikti.Text)
+    End Sub
+
+    Private Sub ÇıktıyıYazdırToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ÇıktıyıYazdırToolStripMenuItem.Click
+        Dim path As String = girdi.Text
+        If (path = "" Or path = String.Empty) Then
+            Return
+        End If
+        Donustur()
+        My.Computer.Clipboard.SetText(cikti.Text)
+        My.Computer.FileSystem.WriteAllText(path, My.Computer.Clipboard.GetText(), False)
+    End Sub
 End Class
